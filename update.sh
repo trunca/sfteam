fbde5a50e16c88e6eea6d6cc2174e30ddd75429a
MastaG <mastag@gmail.com>
2018-02-15 13:38:28 +0100

    Make update.sh actually work..

--- update.sh
+++ update.sh
@@ -3,12 +3,15 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${SCRIPTPATH}"
 git submodule sync
 git submodule update --init
-METAS="$(ls | grep meta- | tr '\n' ' ' | sed 's/ $//g')"
+METAS="$( ls | grep meta- | tr '\n' ' ' | sed 's/ $//g' )"
 cd ..
 # Regenerate bblayers.conf so we can add our own
+rm -f build/conf/bblayers.conf
 make init update
+# Remove existing meta-dream from bblayers.conf
+sed -i "s# $(pwd)/meta-dream##g" build/conf/bblayers.conf
 for i in ${METAS}
 do
-	sed -i "s/\"$/ ${SCRIPTPATH}\/${i}\"/g" build/conf/bblayers.conf
+	sed -E -i "s%^(BBLAYERS =.*)(\"$)%\1 ${SCRIPTPATH}/${i}\"%g" build/conf/bblayers.conf
 done
-sed -i "s/\"$/ ${SCRIPTPATH}\"/g" build/conf/bblayers.conf
+sed -E -i "s%^(BBLAYERS =.*)(\"$)%\1 ${SCRIPTPATH}\"%g" build/conf/bblayers.conf
