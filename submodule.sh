8cf2387c8b3cae5fa7361477f3d68bf47766d8b8
Persian Professionals <persianpros@yahoo.com>
2018-07-24 14:59:44 +0430

    Update submodule.sh

--- submodule.sh
+++ submodule.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo ""
-echo "This is only for MastaG, Persian Prince and Hains with push access"
+echo "This is only for MastaG, Persian Prince, Hains and moham96 with push access"
 echo ""
 echo "Stage 1: git pull for new changes"
 echo ""
